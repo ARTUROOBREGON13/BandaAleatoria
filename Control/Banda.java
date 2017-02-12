200b207b6478cfb695ed950654fc87580eba25a2
ARTUROOBREGON13 <arturoobregon13@gmail.com>
2017-02-12 17:32:42 -0500

    Desarrollo Casi completo
    Falta definir ciertas variables y el diagrama de clases concorde a lo
    programado.

--- Control/Banda.java
+++ Control/Banda.java
@@ -13,14 +13,17 @@ import java.util.ArrayList;
  */
 public class Banda {
 
-	//Constantes para las notas musicales y los nombres de los musicos
+	// Constantes para las notas musicales y los nombres de los musicos
 	public static final String[] Notas = { "Do", "Re", "Mi", "Fa", "Sol", "La", "Si" };
 	public static final String[] Nombres = { "Julio", "Cristiano", "Nicolas", "Tulio", "Renaldo", "Alejandro",
 			"Silvio" };
+	public static final String[] Canciones = { "La cucaracha", "Cucarachita", "Cucarachon" };
 
 	/* Constructor */
 
 	public Banda(int NumeroMusicos) {
+		ListaInstrumentos = new ArrayList<Instrumento>();
+		ListaMusicos = new ArrayList<Musico>();
 		// se incluye cada tipo de instrumento en la lista
 		ListaInstrumentos.add(new Chelo());
 		ListaInstrumentos.add(new Cuatro());
@@ -85,7 +88,8 @@ public class Banda {
 	public void TocarNotaAleatoria() {
 		int a = (int) Math.random() * 7;
 		int i = 1;
-		//Si el instrumento esta afinado, deberia tener la afinacion un valor de 0
+		// Si el instrumento esta afinado, deberia tener la afinacion un valor
+		// de 0
 		for (Musico m : ListaMusicos) {
 			int afinacion = m.getInstrumento().getAfinacion();
 			m.tocarNota(Notas[a + afinacion], i);
@@ -94,11 +98,16 @@ public class Banda {
 	}
 
 	public void Tocar() {
-
+		int a = (int) Math.random() * 3;
+		System.out.println("La banda ha tocado la cancion: " + Canciones[a]);
 	}
 
 	public void VerBanda() {
-
+		int i = 1;
+		for (Musico m : ListaMusicos) {
+			System.out.println(
+					"El musico " + i + " Nombre: " + m.getNombre() + " Instrumento: " + m.getInstrumento().getNombre()+"\n");
+		}
 	}
 
 }
--- Control/Control.java
+++ Control/Control.java
@@ -10,6 +10,10 @@ package Control;
  */
 public class Control {
    public static void main(String[] args){
-       
+       int cantidadMusicos= (int) Math.random()*10;
+       Banda band = new Banda(cantidadMusicos);
+       band.Afinar();
+       band.Tocar();
+       band.VerBanda();
    } 
 }

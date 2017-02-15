85439c741b1a2a875a0e6099b2d5d1a1fe6f0ebc
ARTUROOBREGON13 <arturoobregon13@gmail.com>
2017-02-15 12:28:01 -0500

    Add files via upload

--- src/Control/Control.java
+++ src/Control/Control.java
@@ -1,19 +1,19 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Control;
 
-import Model.Banda;
-
-import java.util.Scanner;
-
+/**
+ * @author Estudiantes
+ */
 public class Control {
-    
-    
-    
    public static void main(String[] args){
-       Scanner sc = new Scanner(System.in); //Se crea el lector
-        
-
-       int NumeroMusicos= sc.nextInt(); 
-       Banda b= new Banda(NumeroMusicos);
+       int cantidadMusicos= (int) Math.random()*10;
+       Banda band = new Banda(cantidadMusicos);
+       band.Afinar();
+       band.Tocar();
+       band.VerBanda();
    } 
 }
